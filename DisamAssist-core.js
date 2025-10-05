16cc44b98055ecfbe3fceaa90bac207b312a222f
MustafaKemal <554074828@qq.com>
2025-10-05 17:59:02 +0800

    revert

--- DisamAssist-core.js
+++ DisamAssist-core.js
@@ -918,17 +918,24 @@
 		var api = new mw.Api();
 		api.get( {
 			'action': 'query',
-			'list': 'search',
-			'srsearch': 'insource:\"' + page + '\" linksto:\"' + page + '\"',
-			'srlimit': cfg.backlinkLimit,
-			'srnamespace': cfg.targetNamespaces.join( '|' )
+			'list': 'backlinks',
+			'bltitle': page,
+			'blredirect': true,
+			'bllimit': cfg.backlinkLimit,
+			'blnamespace': cfg.targetNamespaces.join( '|' )
 		} ).done( function( data ) {
 			// There might be duplicate entries in some corner cases. We don't care,
 			// since we are going to check later, anyway
 			var backlinks = [];
 			var linkTitles = [getCanonicalTitle( page )];
-			$.each( data.query.search, function() {
+			$.each( data.query.backlinks, function() {
 				backlinks.push( this.title );
+				if ( this.redirlinks ) {
+					linkTitles.push( this.title );
+					$.each( this.redirlinks, function() {
+						backlinks.push( this.title );
+					} );
+				}
 			} );
 			dfd.resolve( backlinks, linkTitles );
 		} ).fail( function( code, data ) {
