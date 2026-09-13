e148d7054f2fd741b6c3aed29c82befbb650b34f
MustafaKemal <554074828@qq.com>
2026-09-13 16:18:10 +0800

    fix: 更新标签以包含 DisamAssist

--- DisamAssist-core.js
+++ DisamAssist-core.js
@@ -1306,7 +1306,7 @@
 			watchlist: cfg.watch,
 			minor: minorEdit,
 			bot: botEdit,
-			tags: 'Automation tool'
+			tags: 'Automation tool|DisamAssist'
 		} ).done( function() {
 			dfd.resolve();
 		} ).fail( function( code, data ) {
